f9cc10e94678f2e12e290aab6e5b55f4e7832abf
Roman-Sidorkin <r.sidorkin@gmail.com>
2017-05-30 21:48:05 +0000

    update telnet client

--- other/telnet_client.py
+++ other/telnet_client.py
@@ -1,10 +1,12 @@
 import telnetlib
+import getpass
 from time import sleep
 
 print("Starting Client...")
 host = "192.168.225.72"
-user = "sidorkin_r"
-password = "ghfdjcelbt"
+user = input("Enter username: ")
+password = getpass.getpass()
+ip_regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 timeout = 10
 
 print("Connecting...")
@@ -23,7 +25,27 @@ while True:
     # print(line)
     if b'Current    : image one' in line:
         print("firmware in 1 slot")
+        session.write(
+            b"download firmware_fromTFTP 80.65.17.254 AutoconfigFirm/DGS-1210-28/dgs-1210-28me-a1-6-13-b028-all.hex image_id 2" + b"\n")
+
+        while True:
+            newline = session.read_until(b"\n")
+            sleep(0.1)
+            print(newline)
+            if b'Download firmware...................... Done' in newline:
+                print("firmware download - complete")
+                print("Please wait, programming flash...")
+            if b'programming flash......... Done' in newline:
+                print("programming flash - complete")
         break
+
+        session.write(b"config firmware image_id 2 boot_up" + b"\n")
+        session.write(b"save" + b"\n")
+        session.write(b"reboot force_agree" + b"\n")
+        session.close()
+        sleep(120)
+
+
     elif b'Current    : image two' in line:
         print("firmware in 2 slot")
         session.write(
@@ -39,9 +61,12 @@ while True:
             if b'programming flash......... Done' in newline:
                 print("programming flash - complete")
         break
-        session.write(b"config firmware image_id 1 boot_up" + b"\n")
 
+        session.write(b"config firmware image_id 1 boot_up" + b"\n")
         session.write(b"save" + b"\n")
+        session.write(b"reboot force_agree" + b"\n")
+        session.close()
+        sleep(120)
 
 session.write(b"logout" + b"\n")
 session.close()
