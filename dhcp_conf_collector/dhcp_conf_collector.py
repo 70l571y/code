74856530bdcbdcb70883327793e511364b31ec28
Roman-Sidorkin <r.sidorkin@gmail.com>
2017-06-03 18:13:18 +0000

    test connection

--- dhcp_conf_collector/dhcp_conf_collector.py
+++ dhcp_conf_collector/dhcp_conf_collector.py
@@ -1,7 +1,30 @@
 import psycopg2
+from sshtunnel import SSHTunnelForwarder
+import json
 
 def main():
-    conn_string =
+    with open('/home/sid/PycharmProjects/dhcp/other/config.json', 'r') as f:
+        config = json.load(f)
+    try:
+        with SSHTunnelForwarder(
+                (config['ssh_host'], config['ssh_port']),
+                ssh_password=config['password'],
+                ssh_username=config['username'],
+                remote_bind_address=('127.0.0.1', 5432)) as server:
+            server.start()
+            print("server connected")
+
+            conn = psycopg2.connect(database="switchbase", user=server.ssh_username, password=server.ssh_password, host="localhost",
+                                    port=server.local_bind_port)
+            curs = conn.cursor()
+            print("database connected")
+
+            # sql = "select * from tabelka"
+            # curs.execute(sql)
+            # rows = curs.fetchall()
+            # print(rows)
+    except:
+        print("Connection Failed")
 
 if __name__ == "__main__":
 	main()
