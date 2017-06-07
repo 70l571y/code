78133e7fe2c3b78464215cf92f5adf2cdc68f500
Roman-Sidorkin <r.sidorkin@gmail.com>
2017-06-07 20:49:08 +0000

    small refactoring

--- other/tv_analyzer_monitoring.py
+++ other/tv_analyzer_monitoring.py
@@ -4,10 +4,9 @@ import urllib.request
 import urllib.parse
 
 def send_sms(text):
-    # text_sms = text_sms.replace(" ", "+")
     sms_text = {text}
     enc_sms_text = urllib.parse.urlencode(sms_text)
-    url = f"https://sms.ru/sms/send?api_id=1263F23C-04F1-D64A-EAAD-146773D980FC&to=79059743304&msg="
+    url = "https://sms.ru/sms/send?api_id=1263F23C-04F1-D64A-EAAD-146773D980FC&to=79059743304&msg="
     f = urllib.request.urlopen(url + enc_sms_text)
 
 def main():
@@ -18,9 +17,9 @@ def main():
                                        DestHost="172.23.104.1",
                                        Community="public")
         for i in range(len(signalStrength)):
-            if int(signalStrength[i]) > 200:
+            if int(signalStrength[i]) > 210:
                 time.sleep(60)
-                if int(signalStrength[i] > 200):
+                if int(signalStrength[i] > 210):
                     currentSignalStrength = int(signalStrength[i]) / 10
                     sms_text = f"Соколовская 76а: высокий уровень сигнала на анализаторе : {currentSignalStrength} dBuV"
                     send_sms(sms_text)
