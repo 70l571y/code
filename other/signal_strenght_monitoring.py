d417837648834eb06234d9217eec96f112bbbed4
Roman-Sidorkin <r.sidorkin@gmail.com>
2017-06-10 17:42:48 +0000

    daemonizing signal_strenght

--- other/signal_strenght_monitoring.py
+++ other/signal_strenght_monitoring.py
@@ -2,6 +2,11 @@ import netsnmp
 import time
 import urllib.request
 import urllib.parse
+from daemon import daemon_exec
+
+fn = '/tmp/roman/signal_strenght'
+out = {'stdout': fn + '.log'}
+action = 'stop'
 
 def send_sms(text):
     sms_text = {text}
@@ -12,28 +17,25 @@ def send_sms(text):
 def main():
     oid = netsnmp.Varbind('enterprises.32108.2.4.3.3.1.2')
     while True:
-        signalStrength = netsnmp.snmpwalk(oid,
-                                       Version=1,
-                                       DestHost="172.23.104.1",
-                                       Community="public")
+        signalStrength = netsnmp.snmpwalk(oid, Version=1, DestHost="172.23.104.1", Community="public")
         for i in range(len(signalStrength)):
             if int(signalStrength[i]) > 210:
                 time.sleep(60)
-                if int(signalStrength[i] > 210):
-                    currentSignalStrength = int(signalStrength[i]) / 10
-                    sms_text = f"Соколовская 76а: высокий уровень сигнала на анализаторе : {currentSignalStrength} dBuV"
-                    send_sms(sms_text)
-                    while True:
-                        for k in range(len(signalStrength)):
-                            if int(signalStrength[k]) < 200:
-                                time.sleep(60)
-                                if int(signalStrength[k] < 200):
-                                    currentSignalStrength = int(signalStrength[i]) / 10
-                                    sms_text = f"Соколовская 76а: низкий уровень сигнала на анализаторе : {currentSignalStrength} dBuV"
-                                    send_sms(sms_text)
-                                    break
+                currentSignalStrength = int(signalStrength[i]) / 10
+                sms_text = f"Соколовская 76а: высокий уровень сигнала на анализаторе : {currentSignalStrength} dBuV"
+                send_sms(sms_text)
+                time.sleep(60)
+                while True:
+                    for k in range(len(signalStrength)):
+                        if int(signalStrength[k]) < 200:
+                            currentSignalStrength = int(signalStrength[i]) / 10
+                            sms_text = f"Соколовская 76а: низкий уровень сигнала на анализаторе : {currentSignalStrength} dBuV"
+                            send_sms(sms_text)
+                            break
                         time.sleep(60)
+
+
         time.sleep(60)
 
 if __name__ == '__main__':
-    main()
+    daemon_exec(main, action, fn + '.pid', **out)
