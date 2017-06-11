7ab963ccba478454f938da96f68bbc251fcab52c
Roman-Sidorkin <r.sidorkin@gmail.com>
2017-06-11 18:05:57 +0000

    first commit

--- dhcp_conf_collector/dhcp_conf_collector.py
+++ dhcp_conf_collector/dhcp_conf_collector.py
@@ -2,7 +2,7 @@ import psycopg2
 from sshtunnel import SSHTunnelForwarder
 import json
 
-def main():
+def sql_request(sql_request):
     with open('/home/sid/PycharmProjects/dhcp/other/config.json') as f:
         config = json.load(f)
     try:
@@ -13,18 +13,21 @@ def main():
                 remote_bind_address=('127.0.0.1', 5432)) as server:
             server.start()
             print("server connected")
-
             conn = psycopg2.connect(database="switchbase", user=server.ssh_username, password=server.ssh_password, host="localhost",
                                     port=server.local_bind_port)
             curs = conn.cursor()
             print("database connected")
-
-            # sql = "select * from tabelka"
-            # curs.execute(sql)
-            # rows = curs.fetchall()
-            # print(rows)
+            curs.execute(sql_request)
+            rows = curs.fetchall()
+            return rows
     except:
         print("Connection Failed")
+
+def main():
+    sql = "select * from switches where switch_data @>'{\"mac\": \"30-71-B2-61-C3-EF\"}';"
+    # Перечисления столбцов: id   | model_id | tkd_id | subnet_id | allocation_id | switch_data | users | creation_date | transport_data | topo | migrated
+    # [(16544, 333, 5618, 328, 1, {'ip': '192.168.19.50', 'mac': '30-71-B2-61-C3-EF', 'serial': '1602260047N0240', 'location': '', 'original_mac': '30-71-B2-61-C3-EF'}, {'creator_id': '553'}, datetime.datetime(2016, 8, 19, 16, 18, 4, 274144), {}, '16544', False)]
+    print(sql_request(sql))
 
 if __name__ == "__main__":
 	main()
