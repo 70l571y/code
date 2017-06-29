7582e2ec855910fd67c9a9aad8858b27f15c7b38
70l571y <r.sidorkin@gmail.com>
2017-06-29 16:37:05 +0000

    small ref

--- dhcp_conf_collector/dhcp_conf_collector.py
+++ dhcp_conf_collector/dhcp_conf_collector.py
@@ -42,11 +42,11 @@ def sql_request(sql):
 def add_conf_entry(mac_address):
     network_settings = get_network_settings(mac_address)
     reverse_ip = network_settings[0].replace('.', ',')
-    write_entry = "subnet " + network_settings[1] + " netmask " + network_settings[
-        2] + " {\nauthoritative;\noption routers 109.226.250.11;\n" \
+    write_entry = "subnet " + network_settings[1] + " netmask " + \
+            network_settings[2] + " {\nauthoritative;\noption routers 109.226.250.11;\n" \
              "deny unknown-clients;\noption rfc3442-classless-static-routes 24,109,226,250," + reverse_ip + ";\n" \
-                                                                                                            "host krk250981 {\nhardware ethernet 00:25:11:c3:38:ef ;\nfixed-address " + \
-                  network_settings[0] + " ;\n}\n}\n}"
+             "host krk250981 {\nhardware ethernet 00:25:11:c3:38:ef ;\nfixed-address " + \
+             network_settings[0] + " ;\n}\n}\n}"
     with open(production_config_file, 'r') as dhcpd_conf_file:
         config_file = dhcpd_conf_file.readlines()
 
